31e6087c98e40771a22d61b4e2599cb3da47b179
Taehoon Kim <carpedm20@gmail.com>
2014-08-20 14:30:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+torrenty
+========
+
+*Use the Torrent, Luke...*
+
+I just want to download a movie with python
+
+
+License
+-------
+
+Source codes are distributed under BSD license.
+
+
+Author
+------
+
+Taehoon Kim / [@carpedm20](http://carpedm20.github.io/about/)
---
+++ torrenty/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+    torrenty
+    ~~~~
+
+    Use the Torrent, Luke...
+
+    :copyright: (c) 2014 by Taehoon Kim.
+    :license: BSD, see LICENSE for more details.
+"""
+
+__copyright__ = 'Copyright 2014 by Taehoon Kim'
+__version__ = '0.0.1'
+__license__ = 'BSD'
+__author__ = 'Taehoon Kim'
+__author_email__ = 'carpedm20@gmail.com'
+__url__ = 'http://github.com/carpedm20/torrenty'
+
+__all__ = [
+]
